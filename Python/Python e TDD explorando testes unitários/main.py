d412aae722cf6b8f52cebb850034eb6bc2b3525f
Bruno Oliveira <bruoli3@gmail.com>
2023-04-16 17:29:41 -0300

    Estudando Django

--- Python/Python e TDD explorando testes unitários/main.py
+++ Python/Python e TDD explorando testes unitários/main.py
@@ -1,5 +1,7 @@
 from codigo.bytebank import Funcionario
 
-lucas = Funcionario('Lucas Carvalho', '13/03/2000', 1000)
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/2000', 1000)
+    print(f'Teste = {funcionario_teste.idade()}')
 
-print(lucas.idade())
+teste_idade()
